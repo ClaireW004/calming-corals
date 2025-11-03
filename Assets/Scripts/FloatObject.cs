0b8eb9c4360cc4b95bbfed726633e7bdcfa8203e
Joyin Ang <joyinang2022@gmail.com>
2025-11-02 19:15:38 -0500

    adding trash

---
+++ Assets/Scripts/FloatObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatObject : MonoBehaviour
+{
+    public float speed = 2f; // How fast the object floats
+    public float height = 0.5f; // How high the object floats
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
